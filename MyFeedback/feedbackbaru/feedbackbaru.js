6b3ae56799617b43f51be35501b6256b49d3a872
raditt <125120248+Radityaghifari@users.noreply.github.com>
2025-03-27 13:07:51 +0700

    Delete MyFeedback directory

--- MyFeedback/feedbackbaru/feedbackbaru.js
+++
@@ -1,9 +0,0 @@
-// FeedbackBaru.js
-document.addEventListener('DOMContentLoaded', function() {
-    const cancelButton = document.getElementById('cancelButton');
-
-    cancelButton.addEventListener('click', function(event) {
-        event.preventDefault(); // Prevent form submission
-        window.location.href = '../myfeedback/myfeedback.html'; // Redirect to MyFeedback.html
-    });
-});
