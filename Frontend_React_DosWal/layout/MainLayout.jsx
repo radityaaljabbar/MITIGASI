aff36d8e6e3d77329c73b211344f5ffbb31e1467
Miftah Farid Maulana <miftahfrdmaulana@gmail.com>
2025-04-01 21:00:02 +0700

    Update MainLayout.jsx
    Update MainLayout biar si sidebar ga ikut ke scroll

--- Frontend_React_DosWal/layout/MainLayout.jsx
+++ Frontend_React_DosWal/layout/MainLayout.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Outlet } from 'react-router-dom';
-
+import 'react-toastify/dist/ReactToastify.css';
 import Sidebar from '../components/Sidebar';
 
 const MainLayout = () => {
+    const [sidebarExpanded, setSidebarExpanded] = useState(true);
+
     return (
-        <div className="flex">
-            <Sidebar />
-            <div className="flex-1">
+        <div className="flex h-screen overflow-hidden">
+            <Sidebar
+                expanded={sidebarExpanded}
+                setExpanded={setSidebarExpanded}
+            />
+            <div className="flex-1 overflow-auto bg-[#FAF0E6]">
                 <Outlet />
             </div>
         </div>
