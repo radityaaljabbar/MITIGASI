e9b2b6c73688f4625a3d9abe9abcc07283a25c6f
miftahfrdmaulana <miftahfrdmaulana@gmail.com>
2025-04-09 23:53:22 +0700

    Membuat mainlayout utama nyatu doswal sm mhsiswa

--- FrontEnd_React/src/layout/MainLayoutDosenWali.jsx
+++ FrontEnd_React/src/layout/MainLayoutDosenWali.jsx
@@ -1,18 +1,34 @@
 import React, { useState } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import SidebarMahasiswa from '../components/compMahasiswa/SidebarMahasiswa';
 import SidebarDosenWali from '../components/compDosenWali/SidebarDosenWali';
 
-const MainLayoutDosenWali = () => {
+const MainLayout = () => {
     const [sidebarExpanded, setSidebarExpanded] = useState(true);
+    const location = useLocation();
+
+    // Determine which sidebar to show based on the URL path
+    const isLecturerPath = location.pathname.startsWith('/lecturer');
+    const isStudentPath = location.pathname.startsWith('/student');
 
     return (
         <div className="flex h-screen overflow-hidden">
-            <SidebarDosenWali
-                expanded={sidebarExpanded}
-                setExpanded={setSidebarExpanded}
-            />
+            {isLecturerPath && (
+                <SidebarDosenWali
+                    expanded={sidebarExpanded}
+                    setExpanded={setSidebarExpanded}
+                />
+            )}
+
+            {isStudentPath && (
+                <SidebarMahasiswa
+                    expanded={sidebarExpanded}
+                    setExpanded={setSidebarExpanded}
+                />
+            )}
+
             <div className="flex-1 overflow-auto bg-[#FAF0E6]">
                 <Outlet />
                 <ToastContainer />
@@ -21,4 +37,4 @@ const MainLayoutDosenWali = () => {
     );
 };
 
-export default MainLayoutDosenWali;
+export default MainLayout;
