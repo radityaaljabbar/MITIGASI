5f27b16c245eae7767c65efbaf5cc8c7e53504ec
Radityaghifari <radityaghifar8@gmail.com>
2025-04-12 11:27:45 +0700

    Import handleBaack di NotFoundPage

--- src/pages/lecturer/NotFoundPage.jsx
+++ src/pages/lecturer/NotFoundPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router';
+import handleBack from '../../components/handleBack';
 import { FaExclamationTriangle } from 'react-icons/fa';
 
 const NotFoundPage = () => {
@@ -8,11 +8,11 @@ const NotFoundPage = () => {
             <FaExclamationTriangle className="fas fa-exclamation-triangle text-yellow-400 text-6xl mb-4" />
             <h1 className="text-6xl font-bold mb-4">404 Not Found</h1>
             <p className="text-xl mb-5">This page does not exist</p>
-            <Link
-                to="/"
+            <button
+                onClick={handleBack}
                 className="text-white bg-[#951A22] hover:bg-[#951a22c2] rounded-md px-3 py-2 mt-4">
                 Go Back
-            </Link>
+            </button>
         </section>
     );
 };
