e721510f59b94757b3606a62c7fcf0bf1672360c
Radityaghifari <radityaghifar8@gmail.com>
2025-04-15 11:47:53 +0700

    hapus fitur export

--- src/pages/lecturer/MyReport/StudentListView.jsx
+++ src/pages/lecturer/MyReport/StudentListView.jsx
@@ -61,11 +61,7 @@ const StudentListView = ({ onViewDetail }) => {
             </select>
             <Filter size={16} className="absolute right-3 top-3 text-gray-400 pointer-events-none" />
           </div>
-          
-          <button className="flex items-center gap-1 px-3 py-2 bg-green-600 text-white rounded-xl hover:bg-green-700 transition-colors">
-            <Download size={16} />
-            <span className="hidden md:inline">Export</span>
-          </button>
+        
         </div>
       </div>
       
