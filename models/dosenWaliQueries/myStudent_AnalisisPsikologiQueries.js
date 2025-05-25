4e551ba4d0681c7356c37f9f1808c05365f36195
miftahfrdmaulana <miftahfrdmaulana@gmail.com>
2025-05-25 16:06:44 +0700

    Nambah query get semester

--- models/dosenWaliQueries/myStudent_AnalisisPsikologiQueries.js
+++ models/dosenWaliQueries/myStudent_AnalisisPsikologiQueries.js
@@ -4,7 +4,14 @@ exports.getWellnessResult = async (nim) => {
     const [rows] = await pool.execute(
         `SELECT 
             htp.*,
-            m.nama
+            m.nama,
+            m.kelas,
+            COALESCE(
+                (SELECT MAX(p.semester) + 1 
+                 FROM persemester p 
+                 WHERE p.nim_mahasiswa = m.nim), 
+                1
+            ) as current_semester
         FROM hasil_tes_psikologi htp
         JOIN mahasiswa m ON htp.nim = m.nim
         WHERE htp.nim = ?`,
