39363fc6685deed17b5722cde237a93903a53dc7
miftahfrdmaulana <miftahfrdmaulana@gmail.com>
2025-06-13 14:04:27 +0700

    Fix double data  list mahasiswa krna query join

--- models/dosenWaliQueries/myStudent_ListQueries.js
+++ models/dosenWaliQueries/myStudent_ListQueries.js
@@ -18,8 +18,8 @@ exports.getStudentsByClassCodes = async (classCodesList) => {
                 mhs.nama, 
                 mhs.kelas,
                 akd.hasil_klasifikasi,
-                psi.klasifikasi,
-                psi.total_skor,
+                psi_latest.klasifikasi,
+                psi_latest.total_skor,
                 fin.status_finansial,
                 ipkt.ipk_lulus,
                 ipkt.sks_lulus,
@@ -39,7 +39,16 @@ exports.getStudentsByClassCodes = async (classCodesList) => {
             LEFT JOIN ipk_mahasiswa ipkt ON ipkt.nim = mhs.nim
             LEFT JOIN tak_mahasiswa takt ON takt.nim = mhs.nim
             LEFT JOIN klasifikasi_akademik akd ON akd.nim = mhs.nim
-            LEFT JOIN hasil_tes_psikologi psi ON psi.nim = mhs.nim
+            -- SUBQUERY untuk ambil hasil tes psikologi terbaru saja
+            LEFT JOIN (
+                SELECT h1.nim, h1.klasifikasi, h1.total_skor
+                FROM hasil_tes_psikologi h1
+                INNER JOIN (
+                    SELECT nim, MAX(tanggalTes) as max_tanggal
+                    FROM hasil_tes_psikologi
+                    GROUP BY nim
+                ) h2 ON h1.nim = h2.nim AND h1.tanggalTes = h2.max_tanggal
+            ) psi_latest ON psi_latest.nim = mhs.nim
             LEFT JOIN klasifikasi_finansial fin ON fin.nim = mhs.nim
             LEFT JOIN klasifikasi_umum ku ON ku.nim = mhs.nim
             WHERE mhs.kelas = ?`,
@@ -53,14 +62,15 @@ exports.getStudentsByClassCodes = async (classCodesList) => {
             ipk: student.ipk_lulus || '-', // Placeholder if NULL
             sks: student.sks_lulus,
             semester: student.current_semester,
-            tak: student.tak ,
-            status: student.hasil_klasifikasi_umum || "BLANK", // Default status
-            skor_psikologi: student.total_skor || "-",
+            tak: student.tak,
+            status: student.hasil_klasifikasi_umum || 'BLANK', // Default status
+            skor_psikologi: student.total_skor || '-',
             status_fin: student.status_finansial_bin,
             details: {
-                akademik: student.hasil_klasifikasi ,
+                akademik: student.hasil_klasifikasi,
                 psikologis: student.klasifikasi || '-',
-                finansial: student.status_finansial === "1" ? 'bermasalah' : 'aman',
+                finansial:
+                    student.status_finansial === '1' ? 'bermasalah' : 'aman',
             },
         }));
         studentList.push(...formattedStudents);
